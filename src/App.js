3f21f7c4135c37688c8a8f87c172197580cf2c5f
Harriet Thompson <46738131+hazthompson@users.noreply.github.com>
2020-12-13 13:39:01 -0800

    Add apollo client

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { EVENTS_QUERY } from "./utils/queries";
+import { useQuery } from "@apollo/client";
 
 function App() {
+  const { loading, data } = useQuery(EVENTS_QUERY);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Soundtrip
-        </p>
+        <p>Soundtrip</p>
+        <div>
+          {loading
+            ? "loading"
+            : data.events.map((event, index) => <p key={index}>{event.artistName}</p>)}
+        </div>
       </header>
     </div>
   );
---
+++ src/utils/queries.js
@@ -0,0 +1,9 @@
+import { gql } from '@apollo/client';
+
+export const EVENTS_QUERY = gql`
+  query Events {
+    events {
+      artistName
+    }
+  }
+`;
