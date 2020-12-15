8c213e1f50c1eaa8f1aa8c7cf8458ace07765596
Harriet Thompson <46738131+hazthompson@users.noreply.github.com>
2020-12-14 20:22:52 -0800

    Add fonts

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import GlobalStyles from 'assets/GlobalStyles';
 
 const appStyles = makeStyles({
   App: {
+    fontFamily: `${GlobalStyles.bodyFont}`,
     display: 'grid',
     gridTemplateColumns: 'repeat(12, 1fr)',
     backgroundColor: `${GlobalStyles.backgroundColor}`,
     minHeight: '100vh',
   },
   App__header: {
+    fontFamily: `${GlobalStyles.headerFont}`,
+
     gridColumn: '1 /span 12',
     display: 'flex',
     flexDirection: 'column',
@@ -17,7 +20,6 @@ const appStyles = makeStyles({
     justifyContent: 'center',
     fontSize: '30px',
     color: `${GlobalStyles.titleColor}`,
-    fontWeight: 'bolder',
   },
 });
 
@@ -25,9 +27,7 @@ function App() {
   const classes = appStyles();
   return (
     <div className={classes.App}>
-      <header className={classes.App__header}>
-        <p>Soundtrip</p>
-      </header>
+      <header className={classes.App__header}>Soundtrip</header>
       <EventsList />
     </div>
   );
--- src/assets/GlobalStyles.js
+++ src/assets/GlobalStyles.js
@@ -1,6 +1,11 @@
 const GlobalStyles = {
+  //colors:
   titleColor: '#FE8A5A',
   backgroundColor: '#282c34',
+
+  //font:
+  headerFont: 'Poppins, sans-serif',
+  bodyFont: 'Raleway, sans-serif',
 };
 
 export default GlobalStyles;
